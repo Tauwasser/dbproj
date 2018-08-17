a6a6f5e7a1499380ac98c339a941672a8a79f646
Tauwasser <tauwasser@gmail.com>
2018-08-17 11:32:13 +0200

    migrate old url regex URLs to new path URLs
    https://docs.djangoproject.com/en/2.0/releases/2.0/#simplified-url-routing-syntax
    Signed-off-by: Tauwasser <tauwasser@gmail.com>

--- dbproj/urls.py
+++ dbproj/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^dump/', include('dbproj.dumps.urls')),
-    url(r'^pcb/', include('dbproj.pcbs.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('dump/', include('dbproj.dumps.urls')),
+    path('pcb/', include('dbproj.pcbs.urls')),
+    path('admin/', admin.site.urls),
 ]
