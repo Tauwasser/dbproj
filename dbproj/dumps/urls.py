34b22697d12a8750eb5fe994e21bc80f3e7225fa
Tauwasser <tauwasser@gmail.com>
2018-08-17 11:42:31 +0200

    dumps: use jquery for live search of games
    Very WIP, probably not going to use it for a MVP game DB
    
    Signed-off-by: Tauwasser <tauwasser@gmail.com>

--- dbproj/dumps/urls.py
+++ dbproj/dumps/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ajax/find_games/', views.find_games, name='find_games'),
 ]
--- dbproj/dumps/views.py
+++ dbproj/dumps/views.py
@@ -3,9 +3,38 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.forms import model_to_dict
 from .models import Game, GameTitle, Language
+
+
+class ExtendedEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+
+        if isinstance(o, Game) or isinstance(o, GameTitle) or isinstance(o, Language):
+            return model_to_dict(o)
+
+        return super().default(o)
 
 
 def index(request):
     return render(request, 'dumps/index.html', {})
 
+
+def find_games(request):
+    searchTerm = request.GET.get('searchTerm', None)
+    games = Game.objects.filter(gametitle__title__icontains=searchTerm).distinct()
+
+    gamesFound = [
+        {
+            'id': x.id,
+            'name': str(x),
+            'codes': list(set(filter(None, x.gametitle_set.values_list('code', flat=True))))
+        } for x in games]
+
+    data = {
+        'games_found': gamesFound,
+    }
+    return JsonResponse(data, encoder=ExtendedEncoder)
