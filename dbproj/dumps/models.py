525b3a3c3f3a7d97baf94b295105c00e03f0d77d
Tauwasser <tauwasser@gmail.com>
2018-08-17 18:35:01 +0200

    dumps: start over and use uuid for game ID
    Signed-off-by: Tauwasser <tauwasser@gmail.com>

--- dbproj/dumps/models.py
+++ dbproj/dumps/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import uuid
 from django.db import models
 
 
@@ -24,6 +25,7 @@ class System(models.Model):
 
 
 class Game(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     system = models.ForeignKey(System, on_delete=models.PROTECT, db_index=False)
     langs = models.ManyToManyField(Language,
                                    limit_choices_to={'isDisplayLanguage': True},
